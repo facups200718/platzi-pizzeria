3b62828f35e6ba98180f6235925b5753cf73d9eb
Facundo Perez Schneider <facups200718@gmail.com>
2023-09-28 13:14:47 -0300

    Creo entities

---
+++ src/main/java/com/platzi/platzipizzeria/persistence/entity/OrderItemEntity.java
@@ -0,0 +1,40 @@
+package com.platzi.platzipizzeria.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "order_item")
+@IdClass(OrderItemId.class)
+@Getter
+@Setter
+@NoArgsConstructor
+public class OrderItemEntity {
+    @Id
+    @Column(name = "id_order", nullable = false)
+    private Integer idOrder;
+
+    @Id
+    @Column(name = "id_item", nullable = false)
+    private Integer idItem;
+
+    @Column(name = "id_pizza", nullable = false)
+    private Integer idPizza;
+
+    @Column(nullable = false, columnDefinition = "Decimal(2,1)")
+    private Double quantity;
+
+    @Column(nullable = false, columnDefinition = "Decimal(5,2)")
+    private Double price;
+
+    @ManyToOne
+    @JoinColumn(name = "id_order", referencedColumnName = "id_order", insertable = false, updatable = false)
+    private OrderEntity order;
+
+    // Con insertable = false y updatable = false estamos indicando que no queremos que se creen registros en la tabla pizza ni que se actualicen automaticamente
+    @OneToOne
+    @JoinColumn(name = "id_pizza", referencedColumnName = "id_pizza", insertable = false, updatable = false)
+    private PizzaEntity pizza;
+}
