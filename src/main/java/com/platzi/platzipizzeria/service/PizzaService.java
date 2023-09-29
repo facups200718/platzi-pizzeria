d06fd375b1ad00edc0eda4f7664df04466fc6968
Facundo Perez Schneider <facups200718@gmail.com>
2023-09-29 04:09:11 -0300

    Transactional

--- src/main/java/com/platzi/platzipizzeria/service/PizzaService.java
+++ src/main/java/com/platzi/platzipizzeria/service/PizzaService.java
@@ -3,6 +3,8 @@ package com.platzi.platzipizzeria.service;
 import com.platzi.platzipizzeria.persistence.entity.PizzaEntity;
 import com.platzi.platzipizzeria.persistence.repository.PizzaPageSortingRepository;
 import com.platzi.platzipizzeria.persistence.repository.PizzaRepository;
+import com.platzi.platzipizzeria.service.dto.UpdatePizzaPriceDto;
+import com.platzi.platzipizzeria.service.exception.EmailApiException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -10,6 +12,9 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.*;
 
 @RequiredArgsConstructor
@@ -56,6 +61,17 @@ public class PizzaService {
 
     public Boolean exists(Integer idPizza) {
         return this.pizzaRepository.existsById(idPizza);
+    }
+
+    // Propagation.MANDATORY lo que hace es que si no hay transacción, que se arroje excepción (por lo que entendi)
+    @Transactional(noRollbackFor = EmailApiException.class, propagation = Propagation.MANDATORY)
+    public void updatePrice(UpdatePizzaPriceDto dto) {
+        this.pizzaRepository.updatePrice(dto.getPizzaId(), dto.getNewPrice());
+        this.sendEmail();
+    }
+
+    private void sendEmail() {
+        throw new EmailApiException();
     }
 
     public void delete(Integer idPizza) { this.pizzaRepository.deleteById(idPizza); }
