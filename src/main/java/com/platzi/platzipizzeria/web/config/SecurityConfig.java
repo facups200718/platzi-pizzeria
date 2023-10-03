0a949484803a49ce8afe19da888bdc41051fe06f
Mariano Riquelme <mariano.riquelme@claro.com.ar>
2023-10-03 15:18:16 -0300

    Creando mi propia autenticación

--- src/main/java/com/platzi/platzipizzeria/web/config/SecurityConfig.java
+++ src/main/java/com/platzi/platzipizzeria/web/config/SecurityConfig.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -32,5 +38,20 @@ public class SecurityConfig {
                 .httpBasic(); // y que tiene que estar autenticada con http basic
                 // ( base 64 para user:pass donde la pass es la que genera para cada sesion spring security )
         return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService memoryUsers() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder;
     }
 }
